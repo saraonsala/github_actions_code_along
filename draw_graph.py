e957c84a9048b01b46fb8f47b33b8be5e8349f8d
kokchun <kokchungiang@gmail.com>
2025-01-23 10:11:49 +0100

    fixed formatting and linting

--- draw_graph.py
+++ draw_graph.py
@@ -1,22 +1,13 @@
 import pandas as pd
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-
-
-
-
-
-
-
-df = pd.DataFrame(  {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
+df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
 
 print(df)
 
 plt.plot(df["x"], df["y"])
-
-
 
 
 plt.show()
