ddd8f1e4d53bb577625db486f48cabeb26ca4780
kokchun <kokchungiang@gmail.com>
2025-01-23 09:37:28 +0100

    badly formatted code

---
+++ draw_graph.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+
+
+
+
+
+
+
+df = pd.DataFrame(  {"x": np.array([1,2,3]), "y": np.array([2,5 , 8])}    )
+
+print(df)
+
+plt.plot(df["x"], df["y"])
+
+
+
+
+plt.show()
