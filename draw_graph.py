8817e0c46a28d07df1f89edeff9214824259b4c6
kokchun <kokchungiang@gmail.com>
2025-01-23 10:24:55 +0100

    pre-commit fixed files

--- draw_graph.py
+++ draw_graph.py
@@ -1,7 +1,6 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
+import pandas as pd
 
 df = pd.DataFrame({"x": np.array([1, 2, 3]), "y": np.array([2, 5, 8])})
 
